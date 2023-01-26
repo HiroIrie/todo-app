351931d69ac15c8a4fb05f81bce59b3ca26479e8
HiroIrie <h0514c0325@gmail.com>
2023-01-26 14:20:05 +0900

    nodemonのスキーマ作成しました

--- server/models/todo.mjs
+++ server/models/todo.mjs
@@ -1,1 +1,15 @@
-import mongoose from 'mongoose';
+import {Schema,model} from 'mongoose';
+const todoSchema=Schema({
+  todo:{
+    type:String,
+    required:true
+  },
+  completed:{
+    type:Boolean,
+    default:false
+  }
+});
+
+const Todo=model('Todo',todoSchema);
+
+export default Todo;
